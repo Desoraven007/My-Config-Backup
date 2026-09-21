454a4f14100a4eb88d2e9402f70ce5f6127f085c
Desoraven007 <162979149+Desoraven007@users.noreply.github.com>
2026-09-21 14:27:04 +0100

    Delete switchiptv-1.0.8 directory

--- switchiptv-1.0.8/source/ui/list_cell.cpp
+++
@@ -1,9 +0,0 @@
-#include "ui/list_cell.hpp"
-
-ListCell::ListCell() {
-    this->inflateFromXMLRes("xml/cells/list_cell.xml");
-}
-
-brls::RecyclerCell* ListCell::create() {
-    return new ListCell();
-}
